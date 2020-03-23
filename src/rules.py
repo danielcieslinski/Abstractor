91d53314f4145d708f24cf3adaab112d8a1186ee
danielcieslinski <daniel98c@gmail.com>
2020-03-23 21:14:36 +0100

    agh

--- src/rules.py
+++ src/rules.py
@@ -1,25 +1,17 @@
 import numpy as np
 from methodtools import lru_cache
-from learn import FeatureMatrix
-from learn import Task
+from learn import FeatureMatrix, Task
 import utils
-from toolz import juxt, diff
+from toolz import juxt, diff, reduce
 from operator import sub, truediv
 
-# from pampy import match, _
-from fn import _
-from collections import Counter
 from random import choice
 from toolz import compose
 from funcy import walk_values, group_by, once
 from itertools import  product
-from collections import OrderedDict
 """
 for each class make V = (i, j) where m[i,j] == v
 """
-
-
-points = ()
 lm = lambda F, args: list(map(F, *args))
 
 # c = lambda M: [list(map(lambda y,x, M : (y,x) if  M[y,x] == v else None, M) for v in range(10) )]
@@ -45,7 +37,7 @@ class ExtendedMatrix:
         self.sizes = self.classes_size()
         # --------------
 
-    @once
+    # @once
     def produce_all_cords(self):
         ys, xs = self.shape
         ys, xs = range(ys), range(xs)
@@ -56,22 +48,16 @@ class ExtendedMatrix:
         self.Tclasses = group_by(by_val, self.all_coords)
         self.Tclasses = walk_values(set, self.Tclasses)
 
-    def how_much_class_changed(self, tc, m2):
+    def mdiff(self, tc, m2):
         return juxt([truediv, sub])(self.Tclasses[tc], m2.Tclasses[tc])
 
     def classes_size(self):
         return walk_values(len, self.Tclasses)
 
-    # def class_grouping_refresh(self):
-    #     self.make_classes()
-    #     self.Tclasses = self.make_classes()
-    #     self.tc_sizes = self.classes_size()
-
     def most_common_class(self):
         return max(self.sizes, key=self.sizes.get)
 
     # Setters/getters
-
     def __getitem__(self, item):
         return self.M[item]
 
@@ -85,8 +71,11 @@ class ExtendedMatrix:
                 self.Tclasses[value].add(key)
                 self.M[key] = value
 
-        # else: self.bulk_create(key, value)
-        # else: self.
+
+    def __copy__(self):
+        n = ExtendedMatrix(m=self.M.copy())
+        n.chain = self.chain.copy()
+        return n
 
     # Test
     def test(self):
@@ -105,34 +94,45 @@ class ExtendedMatrix:
         print(self.M)
         print(self.Tclasses)
 
-class LearningPair:
-    def __init__(self, IM, OM):
-        self.IM = IM
-        self.OM = OM
-
-    def select_init_learning_M(self):
-        pass
-
-
-
+from skimage.feature import hog
 
 class EasyAgent:
     # Without objects
-    def __init__(self, IM, OM, learned=None, to_learn=None, classes=None):
+    def __init__(self, IM, OM, pair_num):
         self.IM = IM
         self.OM = OM
-        self.L = learned
-        self.to_learn = to_learn
-        self.classes = classes
+        # self.pair_num = pair_num
+        #
+        # #
+        # self.bg_color = self.find_background()
+        # self.L = np.full(self.OM.shape(), self.bg_color)
+        # self.objects = []
 
+        print(self.IM.M)
+
+        fd, hog_image = hog(self.OM.M, orientations=8, pixels_per_cell=(2, 2),
+                            cells_per_block=(1, 1), visualize=True, multichannel=False)
+        print(fd)
 
     def find_background(self):
-        return max(self.OM.Tclasses)
+        return self.IM.most_common_class()
+
+    def make_object(self, c):
+        ay = min(self.IM.Tclasses[c][0], key=lambda t: t[0] )
+        by = max(self.IM.Tclasses[c][0], key=lambda t: t[0] )
+
+        ax = min(self.IM.Tclasses[c][1], key=lambda t: t[1] )
+        bx = max(self.IM.Tclasses[c][1], key=lambda t: t[1] )
+
+        yr, xr = range(ay, by+1), range(ax, bx+1)
+        combs = product(yr, xr)
+        reduce(lambda yx: self.L[yx].set(c), combs)
+        self.objects.append()
 
     def process_input_matrix(self):
-        # Find
-        for c in self.classes:
-            pass
+        for c1 in self.IM.Tclasses:
+            if self.IM.Tclasses[c1] == self.bg_color: pass
+            self.make_object(c1)
 
     def get_available_moves(self):
         """
@@ -156,9 +156,12 @@ def main():
     in_out = [(t.train[i].input.m, t.train[i].output.m) for i in range(len(t.train))]
     in1, out1 = in_out[0]
 
-    em = ExtendedMatrix(in1)
-    em.test()
+    emi = ExtendedMatrix(in1)
+    emo = ExtendedMatrix(out1)
 
+    # print(emo.test())
+
+    EasyAgent(emi, emo, None)
 
 if __name__ == '__main__':
     main()
